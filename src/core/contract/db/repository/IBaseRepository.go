f280fa1eca9803e3bbb1b582fb96f70daf72fcbe
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-08 03:06:31 +0700

    fix<core>: update `IBaseRepository`
    Add `error` as return type

--- src/core/contract/db/repository/IBaseRepository.go
+++ src/core/contract/db/repository/IBaseRepository.go
@@ -3,9 +3,9 @@ package repository
 import "github.com/GoDriveApp/GoDriveApi/src/core/entity"
 
 type IBaseRepository[T entity.BaseEntity] interface {
-	Insert(entity *T)
-	Update(entity *T)
-	Delete(entity *T)
-	DeleteById(id string)
+	Insert(entity *T) error
+	Update(entity *T) error
+	Delete(entity *T) error
+	DeleteById(id string) error
 	GetById(id string) *T
 }
