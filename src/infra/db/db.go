657043563d72ab083f2aef4a8cffe00fe260d8f4
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-08 01:45:58 +0700

    feat<infra>: create `Init` func to init database

---
+++ src/infra/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"github.com/GoDriveApp/GoDriveApi/src/core/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	dbURL = "postgres://postgres:postgrespw@localhost:32768/GoDriveDb"
+)
+
+func Init() (error, *gorm.DB) {
+
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+
+	if err != nil {
+		return err, nil
+	}
+
+	err = db.AutoMigrate(&entity.Account{}, &entity.User{})
+
+	if err != nil {
+		return err, nil
+	}
+
+	return nil, db
+}
