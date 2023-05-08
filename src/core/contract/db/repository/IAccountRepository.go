dab26fa8ba76e2c15350485bbb3dd5e034d078e2
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-08 11:20:59 +0700

    feat<core>: add `IAccountRepository` funcs

--- src/core/contract/db/repository/IAccountRepository.go
+++ src/core/contract/db/repository/IAccountRepository.go
@@ -3,5 +3,11 @@ package repository
 import "github.com/GoDriveApp/GoDriveApi/src/core/entity"
 
 type IAccountRepository interface {
-	IBaseRepository[entity.Account]
+	Insert(entity *entity.Account) error
+	Update(entity *entity.Account) error
+	Delete(entity *entity.Account) error
+	DeleteById(id string) error
+	GetById(id string) *entity.Account
+	IsEmailExist(email string) bool
+	IsUsernameExist(username string) bool
 }
